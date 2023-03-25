4c76de9470e15e83d5051b225e41457c1f83ac97
Greatmystic <supergreatmystic@gmail.com>
2023-03-24 20:18:35 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-
-alert('Javascript is connected!');
-
-
-function testFunction() {
-    alert("javascript file is linked");
+function showContent() {
+    var content = document.getElementById("hidden-content");
+    content.style.opacity = "1";
+    var audio = document.getElementById("audio");
+    audio.play();
+    var box = document.getElementById("box");
+    box.style.display = "none";
+    box.onclick = null;
   }
