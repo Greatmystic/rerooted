aeff828ef7a91d2d568ff05ab41be471ff1f227a
Greatmystic <supergreatmystic@gmail.com>
2023-03-31 00:17:00 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ function showContent() {
     box.style.display = "none";
     box.onclick = null;
   }
+
+  function checkPassword() {
+    var password = document.getElementById("password").value;
+    if (password === "BLUEBEAK") {
+      // Display the content if the correct password is entered
+      document.getElementById("content").style.display = "block";
+      document.getElementById("message").innerHTML = "";
+    } else {
+      // Show an error message if the password is incorrect
+      document.getElementById("message").innerHTML = "Incorrect password. Please try again.";
+    }
+  }
